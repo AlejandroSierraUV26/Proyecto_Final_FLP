7c5a780f163073e4ebbb24c61195cdd205fcfbd7
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2024-06-19 23:31:16 -0500

    Proyecto Terminado

---
+++ auxiliar.rkt
@@ -0,0 +1,103 @@
+#lang racket
+
+(provide to-decimal from-decimal parse-string operation-numerical array-slice is-number?)
+
+;; Converts a number from a given base to decimal.
+(define (to-decimal num base)
+  (cond
+    [(eq? base 'd) num]
+    [(eq? base 'b) (string->number (string-replace (to-string num) "b" "") 2)]
+    [(eq? base 'o) (string->number (string-replace (to-string num) "0x" "") 8)]
+    [(eq? base 'h) (string->number (string-replace (to-string num) "hx" "") 16)]
+    )
+)
+
+;; Converts a decimal number to a given base.
+(define (from-decimal num base)
+  (parse-string  (prefix-base base (cond
+    [(eq? base 'd) (number->string num)]
+    [(eq? base 'b) (number->string num 2)]
+    [(eq? base 'o) (number->string num 8)]
+    [(eq? base 'h) (string-upcase (number->string num 16))]
+    )))
+)
+
+;; Checks if a string contains letters.
+(define (string-contains-letters? str)
+  (regexp-match? #rx"[A-Za-z]" str))
+
+;; Converts a string to a number or symbol.
+(define (parse-string str)
+  (if (string-contains-letters? str)
+      (string->symbol str)
+      (string->number str)))
+
+;; Helper function to add the prefix for a given base.
+(define _prefix-base (lambda (base)
+  (cond
+    [(eq? base 'd) ""]
+    [(eq? base 'b) "b"]
+    [(eq? base 'o) "0x"]
+    [(eq? base 'h) "hx"]
+    )
+  )
+)
+
+;; Adds the prefix for a given base to a number.
+(define prefix-base (lambda (base num)
+  (if (string-contains? num "-")
+    (string-append "-" (_prefix-base base) (string-replace num "-" ""))
+    (string-append (_prefix-base base) num)
+  ))  
+)
+
+;; Converts a value to a string representation.
+(define to-string (lambda (x) 
+  (cond
+    [(string? x) x]
+    [(symbol? x) (symbol->string x)]
+    [(number? x) (number->string x)]
+  )))
+
+;; Extracts the base from a string representation of a number.
+(define extract-base 
+  (lambda (str)
+    (cond
+      [(string-contains? str "b") 'b]
+      [(string-contains? str "0x") 'o]
+      [(string-contains? str "hx") 'h]
+      [else 'd]
+      ))
+)
+
+;; Performs a numerical operation on two numbers.
+(define operation-numerical 
+  (lambda (op num1 num2 boolean?)
+    (let ([base (extract-base (to-string num1))]
+          [num1 (to-decimal num1 (extract-base (to-string num1)))]
+          [num2 (to-decimal num2 (extract-base (to-string num2)))]
+          )
+      (if boolean?
+          (op num1 num2)
+          (from-decimal (op num1 num2) base)
+       )
+    )
+  )
+)
+
+
+
+;; Extracts a slice from an array.
+(define array-slice
+  (lambda (v start end)
+    (let (
+          (len (+ 1 (- end start))))
+      (build-vector len (lambda (i) (vector-ref v (+ i start)))))))
+
+;; Determines whether the given value is a number.
+(define is-number?
+  (lambda (x)
+    (cond
+      [(number? x) #t]
+      [(string? x) (not (eq? (extract-base x) 'd))]
+      [else #f])))
--- proyecto.rkt
+++ proyecto.rkt
@@ -1,4 +1,5 @@
 #lang eopl
+(require "auxiliar.rkt")  
 ;; ============================Primitivas============================
 (define lexica
   '((white-sp
@@ -76,13 +77,6 @@
     (expresion ("func" "(" (separated-list identificador ",") ")" expresion) func-exp)
     (expresion ("call" expresion "(" (separated-list expresion ",") ")") call-exp)
 
-    ;;Instanciación y uso de estructuras
-    (expresion ("new" identificador "(" (separated-list expresion ",") ")") new-struct-exp)
-    (expresion ("get" expresion "." identificador) get-struct-exp)
-    (expresion ("set-struct" expresion "." identificador "=" expresion) set-struct-exp)
-
-    ;;Reconocimiento de patrones
-    (expresion ("match" expresion "{" (arbno regular-exp "=>" expresion) "}") match-exp)
 
     ;;Numero-exp
     (numero-exp (digitoDecimal) decimal-num)
@@ -136,115 +130,8 @@
     
     ;;Estructuras de datos
     (struct-decl ("struct" identificador "{" (arbno identificador) "}") struct-exp)
-
-    ;;Expresiones regulares
-    (regular-exp (identificador "::" identificador) list-match-exp)
-    (regular-exp ("numero" "(" identificador ")") num-match-exp)
-    (regular-exp ("cadena" "(" identificador ")") cad-match-exp)
-    (regular-exp ("boolean" "(" identificador ")") bool-match-exp)
-    (regular-exp ("array" "(" (separated-list identificador ",") ")") array-match-exp)
-    (regular-exp ("empty") empty-match-exp)
-    (regular-exp ("default") default-match-exp)
     )
   )
-
-;; ============================Representacion de Datos============================
-
-;; Implementacion basada en los videos de la clase
-;; https://youtu.be/AUq-XtehFjU?si=VC2UnPN4oTfrq7E6
-
-
-;; Representa un entorno.
-(define-datatype environment environment?
-  (empty-env-record) ; Entorno vacío
-  (extended-env-record ; Entorno extendido
-   (syms (list-of symbol?)) ; Lista de símbolos
-   (vec vector?) ; Vector de valores
-   (env environment?))) ; Referencia al entorno padre
-
-;; Representa una referencia a un valor en el entorno.
-(define-datatype referencia referencia?
-  (a-ref (position integer?) ; Posición del valor en el vector
-         (vec vector?))) ; Vector que contiene los valores
-
-;; Crea un entorno vacío.
-(define empty-env
-  (lambda ()
-    (empty-env-record))) ; Entorno vacío
-
-;; Extiende el entorno agregando símbolos y sus valores correspondientes.
-(define extend-env
-  (lambda (syms vals env)
-    (extended-env-record syms (list->vector vals) env)))
-
-;; Aplica la referencia del entorno a un símbolo y devuelve el valor correspondiente.
-(define apply-env-ref
-  (lambda (env sym)
-    (cases environment env
-      (empty-env-record ()
-                        (eopl:error 'apply-env-ref "No se encuentra la variable ~a" sym))
-      (extended-env-record (syms vals env)
-                           (let ((pos (rib-find-position sym syms)))
-                             (if pos ; Si pos es un número, entonces se encontró la posición.
-                                 (a-ref pos vals)
-                                 (apply-env-ref env sym)))))))
-
-;; Desreferencia una referencia y devuelve el valor al que se refiere.
-(define deref
-  (lambda (ref)
-    (primitive-deref ref)))
-
-;; Desreferencia una referencia y devuelve el valor al que se refiere.
-(define primitive-deref
-  (lambda (ref)
-    (cases referencia ref
-      (a-ref (pos vec)
-             (vector-ref vec pos)))))
-
-;; Establece el valor de una referencia.
-(define setref!
-  (lambda (ref val)
-    (primitive-setref! ref val)))
-
-;; Establece el valor de una referencia.
-(define primitive-setref!
-  (lambda (ref val)
-    (cases referencia ref
-      (a-ref (pos vec)
-             (vector-set! vec pos val)))))
-
-;; Encuentra la posición de un símbolo en una lista.
-(define rib-find-position 
-  (lambda (sym los)
-    (lista-posicion sym los)))
-
-;; Encuentra la posición de un símbolo en una lista.
-(define lista-posicion
-  (lambda (sym los)
-    (lista-index (lambda (sym1) (eqv? sym1 sym)) los)))
-
-;; Encuentra el índice de un elemento en una lista.
-(define lista-index
-  (lambda (pred ls)
-    (let aux ((ls ls) (idx 0)) ; Función auxiliar con contador de índice
-      (cond
-        ((null? ls) #f) ; Caso base: lista vacía
-        ((pred (car ls)) idx) ; Si el predicado es verdadero, devuelve el índice actual
-        (else (aux (cdr ls) (+ idx 1))))))) ; Caso recursivo: avanza en la lista e incrementa el índice
-
-;; Aplica el entorno a un símbolo y devuelve el valor correspondiente.
-(define apply-env
-  (lambda (env sym)
-    (deref (apply-env-ref env sym))))
-
-;; Inicializa el entorno con símbolos y valores predefinidos.
-(define init-env
-  (lambda ()
-    (extend-env
-     '(x y z) ; Símbolos
-     '(1 2 3) ; Valores
-     (empty-env))))
-
 (sllgen:make-define-datatypes lexica gramatica)
 
 (define show-the-datatypes
@@ -279,6 +166,8 @@
       gramatica)))
 
 
+
+;; ============================Evaluación============================
 (define eval-struct 
   (lambda (structs)
     (cases struct-decl structs
@@ -289,8 +178,250 @@
   )
 )
 
-(define eval-expression '')
+(define eval-expression
+  (lambda (exp env)
+    (cases expresion exp
+      ; Expresiones básicas
+      (bool-exp (bool) 
+        (cases bool-expresion bool
+          (true-exp () #T)
+          (false-exp () #F)
+          ))
+      (void-exp () 'void)
+      (var-exp (id) (apply-env env id))
+      (num-exp (num) 
+        (cases numero-exp num
+          (decimal-num (num) num)
+          (octal-num (num) (parse-string num))
+          (bin-num (num) (parse-string num))
+          (hex-num (num) (parse-string num))
+          (float-num (num) num)))
+      (cadena-exp (id1 id2)
+        (letrec
+            [(crear_string (lambda(lids)
+                            (cond
+                              [(null? lids) "" ]
+                              [else( string-append " " (symbol->string(car lids))(crear_string(cdr lids)))])))]
+          (string-append (symbol->string id1)(crear_string id2))
+         )
+       )
+      (decl-exp (dcl) 
+        (cases var-decl dcl
+          (lvar-exp (ids rands body)
+               (let ((args (eval-rands rands env)))
+                 (eval-expression body
+                                  (extend-env ids args env))))
 
+
+          (let-exp (ids rands body) 
+              (let ((args (eval-rands rands env)))
+                (if (contains-set? body)
+                    (eopl:error 'decl-exp "No se puede asignar a una variable en una expresión let")
+                 (eval-expression body
+                                  (extend-env ids args env)))))
+          )
+       )
+
+      ; Listas y arrays
+      (lista-exp(lexps) (
+        map (lambda (exp) (eval-expression exp env)) lexps
+      ))
+      (cons-exp (exp1 exp2) 
+        (cons (eval-expression exp1 env) (eval-expression exp2 env)))
+      (empty-list-exp () '())
+      (array-exp (lexp)
+                 (let (
+                       (evaluated (map (lambda (exp) (eval-expression exp env)) lexp)))
+                       (list->vector evaluated)))
+
+      ; Expresion primitivas
+      (prim-num-exp (exp1 prim exp2) (
+        apply-num-primitive prim (eval-expression exp1 env) (eval-expression exp2 env))
+      )
+      (prim-bool-exp (prim lexps)
+            (apply-bool-primitive prim 
+              (map (lambda (exp) (let ([value (eval-expression exp env)])
+                (if (boolean? value) value
+                (eopl:error 'apply-bool-primitive "No es un valor booleano"))
+              )) lexps)
+            )
+      )
+      (prim-list-exp (prim exp) 
+       (apply-list-primitive prim (eval-expression exp env)) 
+      )
+      (prim-array-exp (prim lexps)
+                      (let (
+                            (args (eval-rands lexps env)))
+                        (apply-array-primitive prim args)))
+      (prim-cad-exp (prim lexps)
+                     (apply-cadena-primitive prim
+                                            (map (lambda (lexps)
+                                                   (eval-expression lexps env))
+                                                 lexps)))
+      ; Condicionales
+      (if-exp (test-exp true-exp false-exp) 
+              (if (eval-expression test-exp env)
+                  (eval-expression true-exp env)
+                  (eval-expression false-exp env)))
+
+      ; Iteradores
+      (for-exp (cond-exp from-exp until-exp by-exp do-exp)
+                (let loop (
+                           (env (extend-env (list cond-exp) (list (eval-expression from-exp env)) env))
+                           (until-val (eval-expression until-exp env))
+                           (by-val (eval-expression by-exp env))
+                           (result (eval-expression (void-exp) env)))
+                  (let ((current-val (apply-env env cond-exp)))
+                    (if (< current-val until-val) 
+                        (let* ((do-val (eval-expression do-exp env))
+                               (new-env (extend-env (list cond-exp) (list (+ current-val by-val)) env)))
+                          (set! result do-val)
+                          (loop new-env until-val by-val result))
+                       result))))
+       (while-exp (cond-exp exp) (
+                                  let loop (
+                                            [cond (eval-expression cond-exp env)]
+                                            )
+                                   (if (not cond) #f
+                                       (begin
+                                         (eval-expression exp env)
+                                         (loop (eval-expression cond-exp env))
+                                         )
+                                       )
+                                   ))
+      
+      ;Switch
+      (switch-exp (cond-exp case-exp lexps default-exp) (
+        let loop (
+          [match-case (eval-expression cond-exp env)]
+          [cases (map (lambda (exp) (eval-expression exp env)) lexps)]
+          [lexps lexps]
+          )
+           (cond
+            [(null? cases) (eval-expression default-exp env)]
+            [(eq? match-case (car cases)) (eval-expression (car lexps) env)]
+            [else (loop match-case (cdr cases) (cdr lexps))]
+            )
+          )
+      )
+
+      ; Secuenciación y asignación
+      (begin-exp (exp exps) 
+                 (let loop (
+                            (acc (eval-expression exp env))
+                             (exps exps))
+                    (if (null? exps) acc
+                        (loop (eval-expression (car exps) env)
+                              (cdr exps)))))
+      (set-exp (id rhs-exp)
+              (eval-expression (begin
+                 (setref!
+                  (apply-env-ref env id)
+                  (eval-expression rhs-exp env))
+                 (void-exp)) env))
+
+      ; Funciones
+      (func-exp (lids exp)
+                  (lambda (args env)
+                    (let (
+                          (new-env (extend-env lids args env))) 
+                          (eval-expression exp new-env))
+                  ))
+      (call-exp (exp args)
+                (let (
+                      (func-val (eval-expression exp env))
+                      (eval-args (map (lambda (arg) (eval-expression arg env)) args)))
+                  (apply func-val (list eval-args env))))
+
+      )
+      
+    ))
+(define lista-index
+  (lambda (pred ls)
+    (let aux ((ls ls) (idx 0)) ; Función auxiliar con contador de índice
+      (cond
+        ((null? ls) #f) ; Caso base: lista vacía
+        ((pred (car ls)) idx) ; Si el predicado es verdadero, devuelve el índice actual
+        (else (aux (cdr ls) (+ idx 1))))))) ; Caso recursivo: avanza en la lista e incrementa el índice
+
+(define contains-set?
+  (lambda (expr)
+    (cases expresion expr
+      (set-exp (id rhs-exp) #t)
+      (begin-exp (id rhs-exp) (contains-set? id))
+      (for-exp (cond-exp from-exp until-exp by-exp do-exp) (contains-set? do-exp))
+      (while-exp (cond-exp exp) (contains-set? exp))
+      (else #f))))
+
+
+;; ============================Primitivas============================
+(define apply-num-primitive
+  (lambda (prim num1 num2)
+    (cases primitiva prim
+      (sum-prim () (operation-numerical + num1 num2 #F))
+      (minus-prim () (operation-numerical - num1 num2 #F))
+      (mult-prim () (operation-numerical * num1 num2 #F))
+      (mod-prim () (operation-numerical modulo num1 num2 #F) )
+      (elevar-prim () (operation-numerical expt num1 num2 #F ))
+      (menor-prim () (operation-numerical < num1 num2 #T))
+      (mayor-prim () (operation-numerical > num1 num2 #T))
+      (menorigual-prim () (operation-numerical <= num1 num2 #T))
+      (mayorigual-prim () (operation-numerical >= num1 num2 #T))
+      (diferente-prim () (not (operation-numerical eq? num1 num2 #T)))
+      (igual-prim () (operation-numerical eq? num1 num2 #T))
+      )
+  ))
+(define apply-bool-primitive
+  (lambda (prim args)
+    (cases primitivaBooleana prim
+      (and-prim () (let loop ([args args])
+                    (if (null? args) #t
+                        (if (not (car args)) #f (loop (cdr args))))))
+      (or-prim () (let loop ([args args])
+          (if (null? args) #f
+              (if (car args) #t (loop (cdr args)))
+          )))
+      (xor-prim () (and (or (car args) (cadr args)) (not (and (car args) (cadr args)))))
+      (not-prim () (not (car args)))
+      )
+    ))
+(define apply-list-primitive
+  (lambda (prim args)
+    (cases primitivaListas prim
+      (first-primList () (car args))
+      (rest-primList () (cdr args))
+      (empty-primList () (null? args))
+    )
+  ))
+(define apply-array-primitive
+  (lambda (prim args)
+    (cases primitivaArray prim
+      (length-primArr () (vector-length (car args)))
+      (index-primArr () (let (
+                              (arr (car args))
+                              (index (cadr args)))
+                           (vector-ref arr index)))
+      (slice-primArr() (let (
+                             (vec (car args))
+                             (start (cadr args))
+                             (end (caddr args)))
+                         (array-slice vec start end)))
+      (setlist-primArr () (let (
+                                (vec (car args))
+                                (pos (cadr args))
+                                (val (caddr args)))
+                            (vector-set! vec pos val)
+                             vec))
+    )
+  ))
+(define apply-cadena-primitive  
+  (lambda (prim args)
+      (cases primitivaCadena prim
+        (concat-primCad() (apply string-append args))
+        (length-primCad() (string-length (car args)))
+        (index-primCad() (string (string-ref (car args)(cadr args)))))))
+
+;; ============================Evaluar expresiones============================
 (define eval-rands
   (lambda (rands env)
     (map (lambda (x) (eval-rand x env)) rands)))
@@ -298,6 +429,84 @@
 (define eval-rand
   (lambda (rand env)
     (eval-expression rand env)))
+
+;; ============================Representacion de Datos============================
+
+;; Implementacion basada en los videos de la clase
+;; https://youtu.be/AUq-XtehFjU?si=VC2UnPN4oTfrq7E6
+
+
+(define-datatype environment environment?
+  (empty-env-record) ; Entorno vacío
+  (extended-env-record ; Entorno extendido
+   (syms (list-of symbol?)) ; Lista de símbolos
+   (vec vector?) ; Vector de valores
+   (env environment?))) ; Referencia al entorno
+
+(define-datatype referencia referencia?
+  (a-ref (position integer?) ; Posición del valor en el vector
+         (vec vector?))) ; Vector que contiene los valores
+
+(define empty-env
+  (lambda ()
+    (empty-env-record))) ; Entorno vacío
+
+(define extend-env
+  (lambda (syms vals env)
+    (extended-env-record syms (list->vector vals) env)))
+
+(define apply-env-ref
+  (lambda (env sym)
+    (cases environment env
+      (empty-env-record ()
+                        (eopl:error 'apply-env-ref "No se encuentra la variable ~a" sym))
+      (extended-env-record (syms vals env)
+                           (let ((pos (rib-find-position sym syms)))
+                             (if pos ; Si pos es un número, entonces se encontró la posición.
+                                 (a-ref pos vals)
+                                 (apply-env-ref env sym)))))))
+
+(define deref
+  (lambda (ref)
+    (primitive-deref ref)))
+
+(define primitive-deref
+  (lambda (ref)
+    (cases referencia ref
+      (a-ref (pos vec)
+             (vector-ref vec pos)))))
+
+(define setref!
+  (lambda (ref val)
+    (primitive-setref! ref val)))
+
+(define primitive-setref!
+  (lambda (ref val)
+    (cases referencia ref
+      (a-ref (pos vec)
+             (vector-set! vec pos val)))))
+
+(define rib-find-position 
+  (lambda (sym los)
+    (lista-posicion sym los)))
+
+(define lista-posicion
+  (lambda (sym los)
+    (lista-index (lambda (sym1) (eqv? sym1 sym)) los)))
+
+
+(define apply-env
+  (lambda (env sym)
+    (deref (apply-env-ref env sym))))
+
+(define init-env
+  (lambda ()
+    (extend-env
+     '(x y z) ; Símbolos
+     '(1 2 3) ; Valores
+     (empty-env))))
+
+
 
 (interpretador)
 
